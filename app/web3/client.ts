088f7d6a27db4e4e8a26ea4e37397ba86e72a3c3
armaanansari121 <armaanm2a@gmail.com>
2024-08-16 22:07:15 +0530

    Access Page Made

--- app/web3/client.ts
+++ app/web3/client.ts
@@ -3,7 +3,7 @@ import { createThirdwebClient } from "thirdweb";
 export const idManagementAddress = "0xF3977BD2F5F4B61D7a227FcA2fF73F3ab71F02B3";
 export const geolocationAddress = "0x68aCe34c2B3e8687f885087965974b8302AB0A83";
 export const checkpointManagementAddress =
-  "0x6d219328c2F150E4ec62adFBC44870881D0C79C0";
+  "0x404a9f6CdCa2651f06a231024c562C3da0eeee03";
 
 export const client = createThirdwebClient({
   clientId: "862504c5cb8295e0db634f77f0694835",
